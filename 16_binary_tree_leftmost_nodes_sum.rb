4b9194f3c6f26453a83e3a83fbdb3a3571ef870c
jacobrees <jacob.rees97@yahoo.com>
2020-10-17 13:36:50 +0100

    Add 16_binary_tree_leftmost_nodes_sum.rb

---
+++ 16_binary_tree_leftmost_nodes_sum.rb
@@ -0,0 +1,14 @@
+def leftmost_nodes_sum(array)
+  i = 0
+  sum = 0
+  loop do
+    break if i >= array.length
+
+    sum += array[i]
+    i = (2 * i) + 1
+  end
+  sum
+end
+
+puts leftmost_nodes_sum([2, 7, 5, 2, 6, 0, 9])
+# => 11
