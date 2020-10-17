e5828de4f5cda1fc7be59849e19dbc68d230d071
jacobrees <jacob.rees97@yahoo.com>
2020-10-17 10:58:25 +0100

    Add 13_recursion_binary_search.rb solution

---
+++ 13_recursion_binary_search.rb
@@ -0,0 +1,16 @@
+  def sqrt(number)
+    sqrt_recursive(number, 0, number)
+  end
+  
+  def sqrt_recursive(number, min, max)
+    target = (min + max)/2
+    return target if target*target == number
+    if target * target > number
+    sqrt_recursive(number, min, max = target)
+    elsif target * target < number
+    sqrt_recursive(number, min = target, max)
+    end
+end
+  
+ puts sqrt(25)
+ puts sqrt(7056)
