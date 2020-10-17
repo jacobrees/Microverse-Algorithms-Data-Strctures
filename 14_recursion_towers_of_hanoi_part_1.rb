79b73332aad5c03ecfa7e075d4fd3cfdba6a77e1
jacobrees <jacob.rees97@yahoo.com>
2020-10-17 10:59:52 +0100

    Add 14_recursion_towers_of_hanoi_part_1.rb solution

---
+++ 14_recursion_towers_of_hanoi_part_1.rb
@@ -0,0 +1,17 @@
+def move(starting, goal)
+intermediate = ([1,2,3]-[starting,goal]).first
+  result = move_text(starting, intermediate)
+  result += move_text(starting, goal)
+  result += move_text(intermediate, goal)
+  result
+end
+
+def move_text(position1, position2)
+  position1.to_s + "->" + position2.to_s + " "
+end
+
+puts move(1, 3)
+# => 1->2 1->3 2->3
+
+puts move(2, 3)
+# => 2->1 2->3 1->3
